48bad30ad88a0132b92034dfdc229bd81abe2a7c
AliakseiSamoshyn(272 <aliakseisamoshyn@gmail.com>
2025-06-13 22:30:51 +0200

    small fix

--- CzujePociag/modules/users/views.py
+++ CzujePociag/modules/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from modules.trains.trains_service import TrainService
 from modules.users.serializers import TicketSerializer
 from modules.users.user_service import UserService
 
@@ -16,5 +17,7 @@ class TicketView(APIView):
         user = UserService().get_user_by_email(request.user.email)
         serializer = TicketSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        if not TrainService().reserve_seat(serializer.validated_data["seat_number"], serializer.validated_data["wagon_number"], serializer.validated_data["train_number"]):
+            return Response({"error": "Seat is already reserved"}, status=400)
         ticket = serializer.save(user=user)
         return Response({"content": TicketSerializer(ticket).data})
